fb398a96aa45f93c946f547c7760f3a24cf18f8a
Purv2583 <purvpatel9805@gmail.com>
2024-06-25 10:00:05 +0000

    Sync LeetCode submission Runtime - 83 ms (9.73%), Memory - 73.2 MB (27.65%)

--- solutions/1370-count-number-of-nice-subarrays/solution.cpp
+++ solutions/1370-count-number-of-nice-subarrays/solution.cpp
@@ -1,35 +1,28 @@
-// class Solution {
-// public:
-//     int numberOfSubarrays(vector<int>& nums, int k) {
-//         int n=nums.size(),count=0;
-//         for(int i=0;i<n;i++){
-//             if(nums[i]%2==0){
-//                 nums[i]=0;
-//             }
-//             else{
-//                 nums[i]=1;
-//                 count++;
-//             }
-//         }
-//         for(int i=0;i<n;i++){
-//             for(int j=i+1;j<)
-//         }
-//     }
-// };
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> cnt(n + 1, 0);
-        cnt[0] = 1;
-        int ans = 0, t = 0;
-        for (int v : nums) {
-            t += v & 1;
-            if (t - k >= 0) {
-                ans += cnt[t - k];
-            }
-            cnt[t]++;
+        int oddNo = 0;
+        for(auto val:nums)
+            oddNo += val%2 ? 1 : 0;
+        vector<int> prefixEven(oddNo,0);
+        vector<int> suffixEven(oddNo,0);
+        
+        int evenSoFar=0, oddIndex=0;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]%2) { prefixEven[oddIndex++]=evenSoFar+1; evenSoFar=0; }
+            else evenSoFar++;
         }
-        return ans;
+
+        evenSoFar=0; oddIndex=oddNo-1;
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i]%2) { suffixEven[oddIndex--]=evenSoFar+1; evenSoFar=0; }
+            else evenSoFar++;
+        }
+
+        int ret=0, i=0, j=k-1;
+        while(j<oddNo)
+            ret += (prefixEven[i++]*suffixEven[j++]);
+        
+        return ret;
     }
-};
+};   
