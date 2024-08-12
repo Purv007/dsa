0a4fda2ac4bf4c52efbaa36102b283bdf857978f
Purv2583 <purvpatel9805@gmail.com>
2024-08-12 17:20:16 +0000

    Sync LeetCode submission Runtime - 30 ms (33.76%), Memory - 23.7 MB (23.90%)

---
+++ solutions/0200-number-of-islands/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid){
+        int rows=grid.size();
+        int cols=grid[0].size();
+        
+        vector<vector<bool>>visited(rows,vector<bool>(cols,false));
+        int islands=0;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    bfs(grid,visited,i,j);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+
+    void bfs(vector<vector<char>>& grid,vector<vector<bool>>&visited,int x,int y){
+        int rows=grid.size();
+        int cols=grid[0].size();
+        queue<pair<int,int>>q;
+        q.push({x,y});
+        visited[x][y]=true;
+
+        vector<pair<int,int>>dir={{0,1},{1,0},{0,-1},{-1,0}};
+
+        while(!q.empty()){
+            auto[curX,curY]=q.front();
+            q.pop();
+
+            for(auto[dx,dy]:dir){
+                int newX=curX+dx;
+                int newY=curY+dy;
+
+                if(newX>=0 && newX<rows && newY>=0 && newY<cols && grid[newX][newY]=='1' &&
+                !visited[newX][newY]){
+                    visited[newX][newY]=true;
+                    q.push({newX,newY});
+                }
+            }
+        }
+    }
+};
