76541971b5230248b78b49976543d1e88db692c1
Purv2583 <purvpatel9805@gmail.com>
2024-08-19 07:15:17 +0000

    Sync LeetCode submission Runtime - 3 ms (50.05%), Memory - 7.5 MB (100.00%)

---
+++ solutions/0650-2-keys-keyboard/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int steps=0;
+        int d=2;
+        while(n>1){
+            while(n%d==0){
+                steps+=d;
+                n/=d;
+            }
+            d++;
+        }
+        return steps;
+    }
+};
