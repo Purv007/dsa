d841cfa42fcc96ab5b12df6da36eca70e51a1b60
Purv2583 <purvpatel9805@gmail.com>
2024-09-02 07:15:31 +0000

    Sync LeetCode submission Runtime - 80 ms (5.75%), Memory - 76.9 MB (100.00%)

---
+++ solutions/2006-find-the-student-that-will-replace-the-chalk/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n=chalk.size();
+        long long sum=0;
+        for(auto i:chalk){
+            sum+=i;
+            if(sum>k) break;
+        }
+        k=k%sum;
+        for(int i=0;i<n;i++){
+            k-=chalk[i];
+            if(k<0){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
