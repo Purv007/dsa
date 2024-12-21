f804dfe1aaabbe02c34287779e705bc395220905
Purv2583 <purvpatel9805@gmail.com>
2024-12-21 17:28:01 +0000

    Sync LeetCode submission Runtime - 409 ms (5.54%), Memory - 84.1 MB (13.95%)

--- solutions/2493-reverse-odd-levels-of-binary-tree/solution.cpp
+++ solutions/2493-reverse-odd-levels-of-binary-tree/solution.cpp
@@ -1,23 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* reverseOddLevels(TreeNode* root) {
-        traverseDFS(root->left, root->right, 0);
+        if(!root) return NULL;
+        
+        queue<TreeNode*>nodes;
+        nodes.push(root);
+        int level=0;
+        while(!nodes.empty()){
+            int size=nodes.size();
+            vector<TreeNode*> currLevelNodes;
+            for(int i=0;i<size;i++){
+                TreeNode* node=nodes.front();
+                nodes.pop();
+                currLevelNodes.push_back(node);
+                if(node->left) nodes.push(node->left);
+                if(node->right) nodes.push(node->right);
+            }
+            cout<<level<<endl;
+            for(auto i:currLevelNodes)
+                cout<<i->val<<" ";
+            cout<<endl;
+            if(level%2==1){
+                int left=0,right=currLevelNodes.size()-1;
+                while(left<right){
+                    int temp=currLevelNodes[left]->val;
+                    currLevelNodes[left]->val=currLevelNodes[right]->val;
+                    currLevelNodes[right]->val=temp;
+                    left++;
+                    right--;
+                }
+            }
+            level++;
+        }
         return root;
-    }
-
-private:
-    void traverseDFS(TreeNode* leftChild, TreeNode* rightChild, int level) {
-        if (leftChild == nullptr || rightChild == nullptr) {
-            return;
-        }
-        // If the current level is odd, swap the values of the children.
-        if (level % 2 == 0) {
-            int temp = leftChild->val;
-            leftChild->val = rightChild->val;
-            rightChild->val = temp;
-        }
-
-        traverseDFS(leftChild->left, rightChild->right, level + 1);
-        traverseDFS(leftChild->right, rightChild->left, level + 1);
     }
 };
