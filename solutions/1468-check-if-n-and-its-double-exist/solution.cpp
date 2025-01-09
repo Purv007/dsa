ff46d28ce4fbfa834362e634abeeb2b768c60c7e
Purv2583 <purvpatel9805@gmail.com>
2025-01-09 08:05:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.8 MB (43.28%)

---
+++ solutions/1468-check-if-n-and-its-double-exist/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int>seen;
+        for(auto i:arr){
+            if(seen.count(i*2) || (i%2==0 && seen.count(i/2))) return true;
+            seen.insert(i);
+        }
+        return false;
+    }
+};
