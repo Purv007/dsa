a3dd274ef4ad3eda9df533181ef953bf569935b5
Purv2583 <purvpatel9805@gmail.com>
2025-02-22 12:18:47 +0000

    Sync LeetCode submission Runtime - 93 ms (45.20%), Memory - 10.2 MB (46.29%)

--- solutions/2802-find-the-punishment-number-of-an-integer/solution.cpp
+++ solutions/2802-find-the-punishment-number-of-an-integer/solution.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    bool valid(int num,int target){
-        if(target<0 || num<target) return false;
-        
-        if(num==target) return true;
-
-        return valid(num/10,target-num%10) || valid(num/100,target-num%100) || valid(num/1000,target-num%1000) ; 
-    }
     int punishmentNumber(int n) {
-        int res=0;
-        for(int i=1;i<=n;i++){
-            int sq=i*i;
-            if(valid(sq,i)){
-                res+=(sq);
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (canPartition(to_string(i * i), 0, i)) {
+                sum += (i * i);
             }
         }
-        return res;
+        return sum;
+    }
+
+    bool canPartition(string num, int index, int target) {
+        if (index == num.length()) return target == 0;
+
+        int sum = 0;
+        for (int i = index; i < num.length(); i++) {
+            sum = sum * 10 + (num[i] - '0');
+            if (sum > target) break;
+            if (canPartition(num, i + 1, target - sum)) return true;
+        }
+        return false;
     }
 };
