116e08824937cde0ee500a64ad054e3b625faa2d
Purv2583 <purvpatel9805@gmail.com>
2025-04-01 09:00:28 +0000

    Sync LeetCode submission Runtime - 39 ms (52.05%), Memory - 64.3 MB (81.45%)

---
+++ solutions/2681-put-marbles-in-bags/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n=weights.size();
+        if(n<=k) return 0;
+        vector<int>v(n-1,0);
+        for(int i=0;i<n-1;i++){
+            v[i]=weights[i]+weights[i+1];
+        }
+
+        sort(v.begin(),v.end());
+
+        long long res=0;
+        for(int i=0;i<k-1;i++){
+            res+=v[n-2-i]-v[i];
+        }
+        return res;
+    }
+};
