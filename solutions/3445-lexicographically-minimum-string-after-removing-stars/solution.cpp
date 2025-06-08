c51897f689bb75e6d61f821f32799b3f3dc8f9ec
Purv2583 <purvpatel9805@gmail.com>
2025-06-08 21:43:56 +0000

    Sync LeetCode submission Runtime - 62 ms (80.69%), Memory - 37.9 MB (23.98%)

--- solutions/3445-lexicographically-minimum-string-after-removing-stars/solution.cpp
+++ solutions/3445-lexicographically-minimum-string-after-removing-stars/solution.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     string clearStars(string s) {
-        int n = s.size();
-        priority_queue<char, vector<char>, greater<char>> pq;  // Min-heap
-        unordered_map<char, vector<int>> m;  // Stores indices of each character
-        vector<bool> v(n, true);  // Marks whether each char in s should be kept
-
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '*') {
-                char temp = pq.top();  // Get smallest char so far
-                pq.pop();
-                int last = m[temp].back();  // Get last occurrence index
-                m[temp].pop_back();
-                v[i] = false;       // Remove '*'
-                v[last] = false;    // Remove corresponding smallest char
+        vector<stack<int>> st(26);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != '*') {
+                st[s[i] - 'a'].push(i);
             } else {
-                pq.push(s[i]);
-                m[s[i]].push_back(i);
+                for (int j = 0; j < 26; j++) {
+                    if (!st[j].empty()) {
+                        s[st[j].top()] = '*';
+                        st[j].pop();
+                        break;
+                    }
+                }
             }
         }
 
-        string result = "";
-        for (int i = 0; i < n; i++) {
-            if (v[i]) result += s[i];
+        string ans;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != '*') {
+                ans.push_back(s[i]);
+            }
         }
-        return result;
+        return ans;
     }
 };
