de06f10a06e25cd52056ca7ae8da68287ae74ec5
Purv2583 <purvpatel9805@gmail.com>
2025-07-26 20:03:00 +0000

    Sync LeetCode submission Runtime - 2453 ms (17.41%), Memory - 35.9 MB (79.70%)

--- solutions/0560-subarray-sum-equals-k/solution.cpp
+++ solutions/0560-subarray-sum-equals-k/solution.cpp
@@ -1,39 +1,20 @@
-// class Solution {
-// public:
-//     int subarraySum(vector<int>& n, int k) {
-//         int l=0,r=0,sum=0,res=0;
-//         while(r<n.size()){
-//             sum+=n[r];
-//             while(sum>k){
-//                 if(l+1<n.size())
-//                     sum-=n[l++];
-//             }
-//             if(sum==k){
-//                 res++;
-//             }
-//             if(sum<k){
-//                 r++;
-//             }
-//         }
-//         return res;
-//     }
-// };
 class Solution {
 public:
-    //PLEASE UPVOTE IF YOU LIKE MY SOLUTION  :)
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        int sum=0,ans=0;
-        mp[sum] = 1;
-        for(auto it:nums){
-            sum += it;
-            int find = sum - k;
-            if(mp.find(find) != mp.end()){
-                ans += mp[find];
-            }
-            mp[sum]++;
+        int n=nums.size(),res=0;
+        vector<int>preSum(n+1);
+        preSum[0]=0;
+        for(int i=1;i<n;i++){
+            preSum[i]=preSum[i-1]+nums[i-1];
         }
-        return ans;
-//PLEASE UPVOTE IF YOU LIKE MY SOLUTION  :)
+
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum==k) res++;
+            }
+        }
+        return res;
     }
 };
