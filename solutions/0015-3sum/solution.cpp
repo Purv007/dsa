3da439ff49b5c311c7902f036fd810f6ee765087
Purv2583 <purvpatel9805@gmail.com>
2025-08-07 20:54:37 +0000

    Sync LeetCode submission Runtime - 45 ms (71.59%), Memory - 30.9 MB (24.78%)

--- solutions/0015-3sum/solution.cpp
+++ solutions/0015-3sum/solution.cpp
@@ -1,70 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
         vector<vector<int>>res;
-        
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            int j=i+1;
-            int k=nums.size()-1;
 
-            while(j<k){
-                int total=nums[i]+nums[j]+nums[k];
-                if(total>0){
-                    k--;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int l=i+1,r=n-1;
+            while(l<r){
+            int sum=nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    vector<int>t={nums[i],nums[l],nums[r]};
+                    res.push_back(t);
+                    l++; r--;
+                    while(l<r && nums[l]==nums[l-1]) l++;
                 }
-                else if(total<0){
-                    j++;
+                else if(sum<0){
+                    l++;
                 }
                 else{
-                    res.push_back({nums[i],nums[j],nums[k]});
-                    j++;
-
-                    while(nums[j]==nums[j-1] && j<k){
-                        j++;
-                    }
+                    r--;
                 }
             }
         }
+        
         return res;
     }
 };
-// class Solution {
-// public:
-//     vector<vector<int>> threeSum(vector<int>& nums) {
-//         vector<vector<int>>res;
-        
-//         int n = nums.size();
-//         sort(nums.begin(), nums.end());
-//         for(int i=0;i<n;i++){
-//             for(int j=i+1;j<n;j++){
-//                 for(int k=j+1;k<n;k++){
-//                     if((nums[i]+nums[j]+nums[k]==0) && (i!=j) && (i!=k) && (j!=k)){
-//                         res.push_back({nums[i],nums[j],nums[k]});
-//                     }
-//                 }
-//             }
-//         }
-//         bool flag;
-//         int m=res.size();
-//         for(int i=0;i<m;i++){
-//             flag=false;
-//             for(int j=i+1;j<m;j++){
-//                 if((res[i][0]==res[j][0]) && (res[i][1]==res[j][1]) && (res[i][2]==res[j][3])){
-//                     flag=true;
-//                 }
-//             }
-//                 if(flag){
-//                     res.erase(res.begin()+2,res.begin()+3);
-//                     m--;
-//                 }
-//         }
-        
-//         return res;
-//     }
-// };
-
