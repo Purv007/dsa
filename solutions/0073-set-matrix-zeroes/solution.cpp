3aed28de2b41c7dbbb191cdfe8e67d33166087e6
Purv2583 <purvpatel9805@gmail.com>
2025-08-27 13:05:28 +0000

    Sync LeetCode submission Runtime - 2 ms (30.89%), Memory - 18.7 MB (99.98%)

--- solutions/0073-set-matrix-zeroes/solution.cpp
+++ solutions/0073-set-matrix-zeroes/solution.cpp
@@ -3,10 +3,10 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int colZero=1;
+        int col=1;
 
         for(int i=0;i<n;i++){
-            if(matrix[i][0]==0) colZero=0;
+            if(matrix[i][0]==0) col=0;
             for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
@@ -17,11 +17,11 @@ public:
 
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=1;j--){
-                if( matrix[i][0]==0 || matrix[0][j]==0){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
-            if(colZero==0) matrix[i][0]=0;
+            if(col==0) matrix[i][0]=0;
         }
     }
 };
