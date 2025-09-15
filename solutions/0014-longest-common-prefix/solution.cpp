5e9db43fe98e64128a24aa8fa758b7eb94842d74
Purv2583 <purvpatel9805@gmail.com>
2025-09-15 18:49:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.9 MB (57.87%)

--- solutions/0014-longest-common-prefix/solution.cpp
+++ solutions/0014-longest-common-prefix/solution.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        if(strs.empty()){
-            return "";
-        }
-        if(strs.size()==1){
-            return strs[0];
-        }
-        string res="";
-        string first=strs[0];
-        int count[200];
-        for(int i=1;i<strs.size();i++){
-            count[i-1]=0;
-            for(int j=0;j<strs[i].size();j++){
-                if(strs[i][j]==first[j]){
-                    count[i-1]++;
-                }
-                else{
-                    break;
-                }
+    string longestCommonPrefix(vector<string>& st) {
+        int n=st.size();
+
+        int res=st[0].size();
+
+        for(int i=1;i<n;i++){
+            int temp=0;
+            for(int j=0;j<st[i].size();j++){
+                if(st[0][j]!=st[i][j]) break;
+                if(temp==res) break;
+                temp++;
             }
+            res=min(temp,res);
         }
-        int min=count[0];
-        for(int i=0;i<strs.size()-1;i++){
-            if(count[i]<min){
-                min=count[i];
-            }
-        }
-        
-        for(int i=0;i<min;i++){
-            res+=strs[0][i];
-        }
-        return res;
+        string s=st[0].substr(0,res);
+        return s;
     }
 };
