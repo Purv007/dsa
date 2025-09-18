c46c39c58a0a97143c4751ff3c6a93a66af9e0ac
Purv2583 <purvpatel9805@gmail.com>
2025-09-18 22:16:21 +0000

    Sync LeetCode submission Runtime - 15 ms (5.61%), Memory - 26.6 MB (5.00%)

--- solutions/2021-remove-all-occurrences-of-a-substring/solution.cpp
+++ solutions/2021-remove-all-occurrences-of-a-substring/solution.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
-    string removeOccurrences(string s, string part) {
-        string stack = "";
-        int i=0, end=s.size(), window=part.size();
-        while(i<end){
-            stack += s[i++];
-            int stackSize = stack.size();
-            if((stackSize >= window) && (stack.substr(stackSize-window, window) == part))
-                stack = stack.substr(0, stackSize-window);
+    bool check(string &part,stack<char>&st){
+        stack<char>temp=st;
+        int m=part.size();
+        for(int i=m-1;i>=0;i--){
+            if(temp.top()!=part[i]){ 
+                return false;
+            }
+            temp.pop();
         }
-        return stack;
+        return true;
+    }
+
+    string removeOccurrences(string s, string part) {
+        int n=s.size(),m=part.size();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            st.push(s[i]);
+
+            if(st.size()>=m && check(part,st)){
+                for(int j=0;j<m;j++) st.pop();
+            }
+        }
+
+        string res="";
+        while(!st.empty()){
+            res=st.top()+res;
+            st.pop();
+        }
+
+        return res;
     }
 };
