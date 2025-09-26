b81ac5777c6799820338625ca0963609dc225e81
Purv2583 <purvpatel9805@gmail.com>
2025-09-26 18:12:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.3 MB (29.34%)

--- solutions/0040-combination-sum-ii/solution.cpp
+++ solutions/0040-combination-sum-ii/solution.cpp
@@ -1,29 +1,29 @@
 class Solution {
-    private:
-    void comSum(vector<int>& c, int target,vector<vector<int>>&res,int index,vector<int>&temp){
-        if(target<0){
+public:
+    vector<vector<int>> res;
+
+    void func(vector<int>& candidates, int target, vector<int>& curr, int ind) {
+        if (target == 0) {
+            res.push_back(curr);
             return;
         }
-        if(target==0){
-            res.push_back(temp);
-        }
-        else{
-            for(int i=index;i<c.size() && target>=c[i];i++){
-                if(i>index && c[i]==c[i-1]){
-                    continue;
-                }
-                temp.push_back(c[i]);
-                comSum(c,target-c[i],res,i+1,temp);
-                temp.pop_back();
-            }
-        }
+        if (ind >= candidates.size() || target < 0) return;
+
+        curr.push_back(candidates[ind]);
+        func(candidates, target - candidates[ind], curr, ind + 1);
+        curr.pop_back();
+
+        int next = ind + 1;
+        while (next < candidates.size() && candidates[next] == candidates[ind]) next++;
+        func(candidates, target, curr, next);
     }
-public:
-    vector<vector<int>> combinationSum2(vector<int>& c, int target) {
-        sort(c.begin(),c.end());
-        vector<vector<int>>res;
-        vector<int>temp;
-        comSum(c,target,res,0,temp);
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> temp;
+        res.clear();
+        func(candidates, target, temp, 0);
         return res;
     }
 };
+
