2e4ef33fbc2f67ef232e2f84896d23a645a08931
Purv2583 <purvpatel9805@gmail.com>
2026-01-09 06:55:20 +0000

    Sync LeetCode submission Runtime - 11 ms (14.67%), Memory - 28.5 MB (39.94%)

---
+++ solutions/0503-next-greater-element-ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>res(n);
+        stack<int>s;
+
+        for(int i=2*nums.size()-1;i>=0;i--){
+            while(!s.empty() && nums[s.top()]<=nums[i%n]){
+                s.pop();
+            }
+            res[i%n]=s.empty()?-1:nums[s.top()];
+            s.push(i%n);
+        }
+
+        return res;
+
+    }
+};
