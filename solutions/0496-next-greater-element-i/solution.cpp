8f335532ab947287dbd6cdebcb2bc1c26a4062b9
Purv2583 <purvpatel9805@gmail.com>
2026-01-09 05:53:52 +0000

    Sync LeetCode submission Runtime - 4 ms (23.05%), Memory - 12.7 MB (62.96%)

--- solutions/0496-next-greater-element-i/solution.cpp
+++ solutions/0496-next-greater-element-i/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>res;
-        stack<int>s;
-        unordered_map<int,int>m;
-        for(auto i:nums2){
-            while(!s.empty() && s.top()<i){
-                m[s.top()]=i;
-                s.pop();
-            }
-            s.push(i);
+        unordered_map<int, int> m;
+        stack<int> s;
+
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!s.empty() && s.top()<=nums2[i]) s.pop();
+            m[nums2[i]]=s.empty()?-1 : s.top();
+            s.push(nums2[i]);
         }
 
-        for(int i:nums1){
-            res.push_back(m.count(i) ? m[i] : -1);
+        vector<int> res;
+        for (int num : nums1) {
+            res.push_back(m[num]);
         }
-        
         return res;
     }
 };
