805052749672517567f5a6c847eb00306ced8854
Purv2583 <purvpatel9805@gmail.com>
2026-02-19 07:01:11 +0000

    Sync LeetCode submission Runtime - 51 ms (49.46%), Memory - 80.2 MB (19.95%)

--- solutions/0131-palindrome-partitioning/solution.cpp
+++ solutions/0131-palindrome-partitioning/solution.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
-vector<vector<string>> res;
-    bool pal(string t){
-        int n=t.size();
-        int m=n/2;
-        for(int i=0;i<=m;i++){
-            if(t[i]!=t[n-i-1]) return false;
-        }
-        return true;
-    }
-    void func(vector<string>&curr,string &s,int ind){
-        if(ind==s.size()){
-            res.push_back(curr);
+    vector<vector<int>>dp;
+    vector<vector<string>>res;
+    vector<string>path;
+
+    void solve(string &s,int st){
+        if(st==s.size()){
+            res.push_back(path);
             return;
         }
 
-        for(int i=ind;i<s.size();i++){
-            string temp=s.substr(ind,i-ind+1);
-            if(temp.size()==1 || pal(temp)){
-                curr.push_back(temp);
-                func(curr,s,i+1);
-                curr.pop_back();
+        for(int i=st;i<s.size();i++){
+            if(dp[st][i]){
+                path.push_back(s.substr(st,i-st+1));
+                solve(s,i+1);
+                path.pop_back();
             }
         }
     }
-
     vector<vector<string>> partition(string s) {
-        res.clear();
-        vector<string>temp;
-        func(temp,s,0);
+        int n=s.size();
+        dp.resize(n,vector<int>(n,0));
+
+        for (int i = 0; i < n; ++i) {
+            dp[i][i] = true;
+        }
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=i+len-1;
+                if(s[i]==s[j] && (len==2 || dp[i+1][j-1])){
+                    dp[i][j]=1;
+                }
+            }
+        }
+        
+        solve(s,0);
         return res;
     }
 };
