f8015334c5bf6327b190b58d7542629e5875609a
Purv2583 <purvpatel9805@gmail.com>
2026-02-22 05:19:23 +0000

    Sync LeetCode submission Runtime - 11 ms (33.58%), Memory - 23.1 MB (91.48%)

---
+++ solutions/1508-longest-happy-prefix/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestPrefix(string s) {
+        int n = s.size();
+        vector<int> lps(n, 0);
+
+        for (int i = 1, len = 0; i < n; ) {
+            if (s[i] == s[len]) {
+                lps[i++] = ++len;
+            } else {
+                if (len != 0)
+                    len = lps[len - 1];
+                else
+                    lps[i++] = 0;
+            }
+        }
+
+        return s.substr(0, lps[n - 1]);
+    }
+};
