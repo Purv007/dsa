08fc47dded8db3333750cdc859f77421251a9b76
Purv2583 <purvpatel9805@gmail.com>
2026-02-27 11:36:57 +0000

    Sync LeetCode submission Runtime - 9 ms (74.56%), Memory - 12.7 MB (51.55%)

--- solutions/0368-largest-divisible-subset/solution.cpp
+++ solutions/0368-largest-divisible-subset/solution.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        if(nums.size()==0){
-            return nums;
-        }
+        int n=nums.size();
+        int res=0;
+        int last_idx=0;
         sort(nums.begin(),nums.end());
-        int flag=1;
-        if(nums[0]==1){
-            flag=0;
-        }
-        else{
-            nums.push_back(1);
-            flag=1;
-        }
-        sort(nums.begin(),nums.end());
-        int i,j;
-        int dp[nums.size()][2];
-        for(i=nums.size()-1;i>=0;i--){
-            dp[i][0]=0;
-            dp[i][1]=i;
-            for(j=i+1;j<nums.size();j++){
-                if((nums[j]%nums[i])==0){
-                    if(dp[j][0]>dp[i][0]){
-                        dp[i][0]=dp[j][0];
-                        dp[i][1]=j;
-                    }  
+        vector<int>dp(n,1),hash(n);
+        for(int i=0;i<n;i++){
+            hash[i]=i;
+            for(int prev=0;prev<i;prev++){
+                if(nums[i]%nums[prev]==0 && dp[i]<1+dp[prev]){
+                    dp[i]=1+dp[prev];
+                    hash[i]=prev;
                 }
             }
-            dp[i][0]++;
+            if(res<dp[i]){
+                res=dp[i];
+                last_idx=i;
+            }
         }
-        i=0;
-        vector<int> t;
-        t.push_back(nums[i]);
-        while(dp[i][1]!=i){
-            i=dp[i][1];
-            t.push_back(nums[i]);
-            
-        }
-        if(flag==1){
-            t.erase(t.begin());
-        }
-        return t;
         
+        vector<int>lis;
+        lis.push_back(nums[last_idx]);
+        while(hash[last_idx]!=last_idx){
+            last_idx=hash[last_idx];
+            lis.push_back(nums[last_idx]);
+        }
+        return lis;
     }
 };
