2f8cb2b426bab3870d6c817a970dad76b22315d0
Purv2583 <purvpatel9805@gmail.com>
2026-03-01 18:47:25 +0000

    Sync LeetCode submission Runtime - 23 ms (4.13%), Memory - 20.7 MB (5.01%)

--- solutions/0014-longest-common-prefix/solution.cpp
+++ solutions/0014-longest-common-prefix/solution.cpp
@@ -1,20 +1,64 @@
+class trie{
+    public:
+    char val;
+    trie* child[26];
+    bool isTerminal;
+    int count;
+    trie(char ch){
+        val = ch;
+        for(int i=0;i<26;i++){
+            child[i] = NULL;
+        }
+        isTerminal=false;
+        count=0;
+    }
+};
+
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& st) {
-        int n=st.size();
-
-        int res=st[0].size();
-
-        for(int i=1;i<n;i++){
-            int temp=0;
-            for(int j=0;j<st[i].size();j++){
-                if(st[0][j]!=st[i][j]) break;
-                if(temp==res) break;
-                temp++;
-            }
-            res=min(temp,res);
+    trie* root=new trie('\0');
+    void insert(trie* root,string s){
+        if(s.size()==0){
+            root->isTerminal=true;
+            return;
         }
-        string s=st[0].substr(0,res);
-        return s;
+
+        int idx=s[0]-'a';
+        trie* child;
+        if(root->child[idx]!=NULL){
+            child=root->child[idx];
+        }
+        else{
+            child=new trie(s[0]);
+            root->count++;
+            root->child[idx]=child;
+        }
+
+        insert(child,s.substr(1));
+    }
+
+    void func(trie* root,string &res){
+        if(root->count!=1 || root->isTerminal) return;
+
+        for(int i=0;i<26;i++){
+            if(root->child[i]!=NULL){
+                res+=root->child[i]->val;
+                func(root->child[i],res);
+                break;
+            }
+        }
+        return;
+    }
+
+    string longestCommonPrefix(vector<string>& strs) {
+        int n=strs.size();
+
+        for(auto i:strs){
+            insert(root,i);
+        }
+        if(root->isTerminal) return "";
+        string res="";
+        func(root,res);
+        return res;
     }
 };
