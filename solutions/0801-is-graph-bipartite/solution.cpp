dc38d2c886106cf35dc8542cc7e394e2b5062fd0
Purv2583 <purvpatel9805@gmail.com>
2026-05-23 17:05:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.4 MB (58.78%)

--- solutions/0801-is-graph-bipartite/solution.cpp
+++ solutions/0801-is-graph-bipartite/solution.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
-vector<int> parent,size;
+vector<int>parent,size;
     int find(int x){
         if(x==parent[x]) return x;
         return parent[x]=find(parent[x]);
     }
-
     bool unite(int x,int y){
         int px=find(x);
         int py=find(y);
 
-        if(px==py) return false;
+        if(px==py) return true;
         if(size[px]>size[py]){
             parent[py]=px;
             size[px]+=size[py];
@@ -20,21 +19,23 @@ vector<int> parent,size;
             size[py]+=size[px];
         }
         return true;
+
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
         parent.resize(n);
         size.resize(n,1);
 
-        for(int i=0;i<n;i++) parent[i]=i;
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
 
         for(int i=0;i<n;i++){
             if(graph[i].empty()) continue;
-
-            int first=graph[i][0];
+            int t=graph[i][0];
             for(auto x:graph[i]){
                 if(find(i)==find(x)) return false;
-                unite(first,x);
+                unite(t,x);
             }
         }
         return true;
