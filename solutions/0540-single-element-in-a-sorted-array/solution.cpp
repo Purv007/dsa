5f6edf37af96a729026e3141ebe1a96c5861c616
Purv2583 <purvpatel9805@gmail.com>
2026-07-15 20:01:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.2 MB (35.64%)

--- solutions/0540-single-element-in-a-sorted-array/solution.cpp
+++ solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -4,21 +4,15 @@ public:
         int n=nums.size();
         if(n==1) return nums[0];
         int l=0,r=n-1;
-        while(l<=r){
+
+        while(l<r){
             int mid=l+(r-l)/2;
-            if(mid==0 && nums[0]!=nums[1]) return nums[mid];
-            if(mid==n-1 && nums[n-1]!=nums[n-2]) return nums[mid];
-
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
-            if(mid%2==0){
-                if(nums[mid-1]==nums[mid]) r=mid-1;
-                else l=mid+1;
-            }else{
-                if(nums[mid]==nums[mid-1]) l=mid+1;
-                else r=mid-1; 
+            if(mid%2==1){
+                mid--;
             }
-
+            if(nums[mid]==nums[mid+1]) l=mid+2;
+            else r=mid;
         }
-        return -1;
+        return nums[r];
     }
 };
