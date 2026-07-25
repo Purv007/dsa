e0f045e070a0ad03f2be160307c0cbced420a482
Purv2583 <purvpatel9805@gmail.com>
2026-07-25 19:02:09 +0000

    Sync LeetCode submission Runtime - 3 ms (37.86%), Memory - 14.1 MB (39.86%)

--- solutions/0040-combination-sum-ii/solution.cpp
+++ solutions/0040-combination-sum-ii/solution.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-
-    void func(vector<int>& candidates, int target, vector<int>& curr, int ind) {
-        if (target == 0) {
+    vector<vector<int>>res;
+    void recur(vector<int>&can,int t,vector<int>&curr,int i){
+        if(t==0){
             res.push_back(curr);
             return;
         }
-        if (ind >= candidates.size() || target < 0) return;
+        if(t<0 || i>=can.size()) return;
 
-        curr.push_back(candidates[ind]);
-        func(candidates, target - candidates[ind], curr, ind + 1);
+        curr.push_back(can[i]);
+        recur(can,t-can[i],curr,i+1);
         curr.pop_back();
 
-        int next = ind + 1;
-        while (next < candidates.size() && candidates[next] == candidates[ind]) next++;
-        func(candidates, target, curr, next);
+        int nxt=i+1;
+        while(nxt<can.size() && can[nxt]==can[i]) nxt++;
+        recur(can,t,curr,nxt);
     }
-
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<int> temp;
-        res.clear();
-        func(candidates, target, temp, 0);
+    vector<vector<int>> combinationSum2(vector<int>& can, int target) {
+        sort(can.begin(),can.end());
+        int n=can.size();
+        vector<int>temp;
+        recur(can,target,temp,0);
         return res;
     }
 };
-
