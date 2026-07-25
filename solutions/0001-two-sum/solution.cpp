b98f52a4a42f0acba6a8156aa8a9f4562e86e96b
Purv2583 <purvpatel9805@gmail.com>
2026-07-25 10:58:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.8 MB (39.23%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        ios_base::sync_with_stdio(0);
-        
-        vector<int> res;
-        map<int, int> mp; // Create a map to store the value-index pairs
-
-        for (int i = 0; i < nums.size(); i++) {
-            int first = nums[i]; // Current number
-            int more = target - first; // The other number needed to reach the target
-
-            if (mp.find(more) != mp.end()) { // If the other number is already in the map
-                res.push_back(i); // Add the current index to the result
-                res.push_back(mp[more]); // Add the other number's index to the result
-                break; // We found a valid pair, so exit the loop
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end()) {
+                return {hash[complement], i};
             }
-            mp[first] = i; // Add the current number and its index to the map
+            hash[nums[i]] = i;
         }
-        return res;
+        // Return an empty vector if no solution is found
+        return {};
     }
 };
