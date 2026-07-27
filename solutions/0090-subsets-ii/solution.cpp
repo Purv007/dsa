6150db926404f7fa5812cd43e0bff55fc47abf7e
Purv2583 <purvpatel9805@gmail.com>
2026-07-27 16:37:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.6 MB (46.54%)

--- solutions/0090-subsets-ii/solution.cpp
+++ solutions/0090-subsets-ii/solution.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
+vector<vector<int>>res;
+
+    void func(vector<int>&nums,vector<int>&curr,int i){
+        if(i==nums.size()){
+            res.push_back(curr);
+            return;
+        }
+
+        curr.push_back(nums[i]);
+        func(nums,curr,i+1);
+        curr.pop_back();
+        
+        int nxt=i+1;
+        while(nxt<nums.size() && nums[i]==nums[nxt]) nxt++;
+        func(nums,curr,nxt);
+
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-        vector<int> curr;
-        backtrack(nums, 0, curr, res);
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int>curr;
+        // res.push_back(curr);
+        func(nums,curr,0);
         return res;
     }
-
-private:
-    void backtrack(vector<int>& nums, int start, vector<int>& curr, vector<vector<int>>& res) {
-        res.push_back(curr);
-        for (int i = start; i < nums.size(); ++i) {
-            if (i > start && nums[i] == nums[i - 1]) continue; // skip duplicates
-            curr.push_back(nums[i]);
-            backtrack(nums, i + 1, curr, res);
-            curr.pop_back();
-        }
-    }
 };
-
